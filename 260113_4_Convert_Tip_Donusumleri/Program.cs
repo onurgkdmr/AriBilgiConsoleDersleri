5210d30a0e9e963ced8feaa987449b878665e23c
PC1011 <PC1011@DESKTOP-52EVHMJ>
2026-01-15 20:08:29 +0300

    Add project files.

---
+++ 260113_4_Convert_Tip_Donusumleri/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _260113_4_Convert_Tip_Donusumleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 260115_1_Aritmetik_Operatorler/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _260115_1_Aritmetik_Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Operatörler, programlama dillerind belirli işlemleri gerçekleştirmek için kullanılan sembollerdir.
+            1-Aritmetik
+            2-Karşılaştırma
+            3-Atama
+            4-Mantıksal
+             */
+
+            // 1) Aritmetik operatörler: +.-.*./,% (% => Mod alma)
+
+            int sayi1 = 57;
+            int sayi2 = 25;
+            int topla = sayi1 + sayi2;
+            int carp = sayi1 * sayi2;
+            int bol = sayi1 / sayi2;
+            int cikart = sayi1 - sayi2;
+
+            // Mod alma nedir?
+
+            // % => Bir sayının başka bir sayıya bölünmesi sonucu kalanı verir.
+
+            int modAl = sayi1 % sayi2; // 57'yi 25'e böldüğümüzde kalan 7'dir.
+            Console.WriteLine("Toplam:" + topla);
+            Console.WriteLine("Çıkartma:" + cikart);
+            Console.WriteLine("Bölme:" + bol);
+            Console.WriteLine("Çarpma:" + carp);
+            Console.WriteLine("Mod Alma:" + modAl);
+
+
+
+        }
+    }
+}
