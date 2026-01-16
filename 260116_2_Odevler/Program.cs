f1ecfd1c03e34157361246265fde0b01bc9b2532
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-16 21:31:11 +0300

    Atama operatörleri ödevi

---
+++ 260116_2_Odevler/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _260116_2_Odevler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan alınan 5 sayı için aşağıdaki işlemleri yapınız ve sonuçları ekranda gösteriniz.
+
+            /*
+            1) 5 sayıdan 2'nin katı olanlar için True, olmayanlar için False verecek şekilde bir işlem düşünüp kodlayınız.
+            2) 1.sayıdan 5.sayıya kadar olan sayıların toplamını bulunuz.
+            3) 1.sayıya 5 ekleyin, 2.sayıdan -10 çıkartın, 3.sayıyı da 5 ile çarparak işlemlerin sonucunu ekranda gösteriniz.
+            NOT: Yeni bir değişken tanımlamadan 3.soruyu yapınız.
+            4) 5 sayının toplam sonucu 9,11,13 e ayrı ayrı bölündüğünü True, False şeklinde hesaplayınız.
+            5) 1., 2., 3. sayıların toplamını, 4.sayıdan 5.sayı çıkartılınca kalan sonuca bölerek sonucu ekranda gösteriniz.
+             */
+        }
+    }
+}
