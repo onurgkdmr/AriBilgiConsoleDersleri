4808d543be9aa05e5d76f5c9946af2669ece1bbd
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-22 21:27:19 +0300

    if else örnek

---
+++ 260122_4_maas_zam/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _260122_4_maas_zam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Kullanıcıdan alınan bir maaş için;
+            Eğer maaş değeri 25.000 ve altında ise %45 zam
+            Eğer maaş değeri 50.000 ve üstünde ise %17 zam
+            Eğer maaş değeri 100.000 ve üstünde ise %9 zam
+            Eğer maaş değeri 150.000 ve üstünde ise %5 zam yapacak şekilde yapılan zam oranını ve yeni maaşı kullanıcıya gösteren yapıyı kodlayınız.
+             */
+        }
+    }
+}
