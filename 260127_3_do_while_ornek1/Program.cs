5e8193d6aa57cf123cc55b47eec58705a33d1a2b
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-27 21:34:07 +0300

    do while örnek

---
+++ 260127_3_do_while_ornek1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _260127_3_do_while_ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Kullanıcıdan alınan 5 sayıdan en büyük sayıyı hesaplayan yapıyı kodlayınız.
+            // 2) Kullanıcıdan negatif sayı alana kadar girilen pozitif sayıların toplamını ve adetini hesaplayan do while yapısını kodlayınız.
+            // 3) Kullanıcıdan 1-100 arasında sayı girmesini isteyiniz. Girilen sayı 1-100 arasında değil ise tekrar sayı girmesini isteyiniz.
+            // Girilen sayı 1-100 arasında ise sayıyı ekranda gösteriniz ve 1-100 arasında 11 sayı girildikten sonra işlemi sonlandırıp girilen 
+            // sayıların toplamını ekranda gösteriniz.
+            // 4) Kullanıcı adı admin, şifresi 12?ab olan bir sistem için 3 defa giriş hakkı tanıyan do while sistemini kodlayınız
+
+
+
+            // SORU-1:
+
+
+
+        }
+    }
+}
