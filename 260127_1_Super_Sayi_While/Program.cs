1f844c96283090bc7b33e075a491a8f059c32894
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-27 20:48:06 +0300

    While Süper Sayı Çözümleri

---
+++ 260127_1_Super_Sayi_While/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _260127_1_Super_Sayi_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kendisi hariç bölünenlerin toplamı kendisini veren sayılara süper sayı denir. 1-100000 arasındaki süper sayıları listeleyiniz.
+
+            int sayi = 0;
+            while (sayi<100000)
+            {
+                int bolenSayi = 1;
+                int toplam = 0;
+                while (sayi>bolenSayi)
+                {
+                    if (sayi%bolenSayi==0)
+                    {
+                        toplam = toplam + bolenSayi;
+                    }
+                    bolenSayi++;
+                }
+                if ( sayi==toplam)
+                {
+                    Console.WriteLine("Süper Sayı:"+sayi);
+                }
+                sayi++;
+            }
+        }
+    }
+}
