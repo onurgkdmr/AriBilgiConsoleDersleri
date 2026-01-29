bf5be5e5bbe780f75caae138084879c553a0945a
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-29 20:27:18 +0300

    for döngüsü

---
+++ 260129_2_for_ornek1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _260129_2_for_ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tek for ile 50-5000 arasındaki 11 ve 13 ün katları sayısı ve katları toplamlarını ayrı ayrı gösteriniz.
+
+            int toplam11 = 0;
+            int sayiAdeti11 = 0;
+            int toplam13 = 0;
+            int sayiAdeti13 = 0;
+            for (int i = 50; i < 5000; i++)
+            {
+                if (i%11==0)
+                {
+                    Console.WriteLine(i);
+                    toplam11=toplam11 + i;
+                    sayiAdeti11++;
+                }
+                if (i%13==0)
+                {
+                    Console.WriteLine(i);
+                    toplam13 = toplam13 + i;
+                    sayiAdeti13++;
+                }
+            }
+            Console.WriteLine("11 için sayı adeti:" + sayiAdeti11);
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("13 için sayı adeti:" + sayiAdeti13);
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Sayıların katları toplamı:" + toplam11);
+            Console.WriteLine("Sayıların katları toplamı:" + toplam13);
+            
+
+      
+        }
+    }
+}
---
+++ 260129_3_for_ornek2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _260129_3_for_ornek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1-15000 arasındaki sayıların 5'in katı olup da 2'nin katı olmayan sayıların sayısı ve toplamlarını hesaplayınız.
+
+            int toplam = 0;
+            int sayiAdeti = 0;
+            
+            for(int i=1;i<15000;i++)
+            {
+                if(i%5==0 && !(i%2==0))
+                {
+                    sayiAdeti++;
+                    toplam = toplam + i;
+                }
+            }
+            Console.WriteLine("Sayı adeti:" + sayiAdeti);
+            Console.WriteLine("Sayıların toplamı:" + toplam);
+        }
+    }
+}
