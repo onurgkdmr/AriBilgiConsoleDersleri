7416dafc2d567109bfac123f6347b26475cc0b9f
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-29 19:15:16 +0300

    Ödev yapıldı (Güncel).

--- 260127_3_do_while_ornek1/Program.cs
+++ 260127_3_do_while_ornek1/Program.cs
@@ -15,7 +15,98 @@
 
             // SORU-1:
 
+            int sayiAdeti = 0;
+            int enbuyukSayi = 0;
+            do
+            {
+                Console.WriteLine(sayiAdeti + 1 + ".sayıyı giriniz?");
+                int sayi1 = Convert.ToInt32(Console.ReadLine());
 
+                if (sayi1 > enbuyukSayi)
+                {
+                    enbuyukSayi = sayi1;
+                }
+                sayiAdeti++;
+
+            } while (sayiAdeti < 5);
+
+            Console.WriteLine("Girilen sayılar içinde en büyük sayı:" + enbuyukSayi);
+
+
+            // SORU-2: 
+
+            int sayiAdeti1 = 0;
+            int toplam = 0;
+            int sayi;
+            do
+            {
+                Console.WriteLine("Pozitif bir sayı giriniz:");
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi > 0)
+                {
+                    toplam = toplam + sayi;
+                    sayiAdeti1++;
+                }
+
+            } while (sayi > 0);
+
+            Console.WriteLine("Sayı adeti:" + sayiAdeti1);
+            Console.WriteLine("Sayıların toplamı:" + toplam);
+
+
+            // SORU-3:
+
+            int girilenSayi = 0;
+            int toplam1 = 0;
+            do
+            {
+                Console.WriteLine("1-100 arasında bir sayı giriniz:");
+                int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+                if (!(sayi3 > 1 && sayi3 < 100))
+                {
+                    Console.WriteLine("Tekrar sayı giriniz:");
+                }
+                else
+                {
+                    Console.WriteLine("Girilen sayı:" + sayi3);
+                    toplam1 = toplam1 + sayi3;
+                    girilenSayi++;
+                }
+            } while (girilenSayi < 11);
+
+            Console.WriteLine("Girilen sayıların toplamı:" + toplam1);
+
+
+            // SORU-4:
+
+            int girisHakki = 0;
+
+            do
+            {
+                Console.WriteLine("Kullanıcı adınızı giriniz:");
+                string kullaniciAdi = Console.ReadLine();
+
+                Console.WriteLine("Şifrenizi giriniz:");
+                string sifre = Console.ReadLine();
+
+                if (kullaniciAdi != "admin" || sifre != "12?ab")
+                {
+                    Console.WriteLine("Hatalı giriş yaptınız, tekrar deneyiniz:");
+                    girisHakki++;
+                }
+                else
+                {
+                    Console.WriteLine("Giriş başarılı!");
+                    break;
+                }
+            } while (girisHakki < 3);
+            
+            if (girisHakki==3)
+            {
+                Console.WriteLine("Giriş hakkınız doldu.");
+            }
 
         }
     }
