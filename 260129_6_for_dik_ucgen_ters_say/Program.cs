231b2de2a1de10686c04eb93ea6c774823e48dde
onurgkdmr <onurgkdmr.55@gmail.com>
2026-01-29 21:32:02 +0300

    for döngüsü

---
+++ 260129_6_for_dik_ucgen_ters_say/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _260129_6_for_dik_ucgen_ters_say
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dik üçgenin karakter sayısı max. alınacak şekilde max. değerden 0'a doğru aynı satırda yazdırınız.
+
+            string yildiz = "*";
+
+            for(int i =1; i<10; i++)
+            {
+                Console.Write(yildiz+"---");
+
+                for (int k = i; k >=0; k--)
+                {
+                    Console.Write(k + ",");
+                }
+                Console.WriteLine();
+                yildiz = yildiz + "*";
+            }
+        }
+    }
+}
