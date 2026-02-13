d9e49fe7f5679cd7a58e2b50386bd2dab8964647
onurgkdmr <onurgkdmr.55@gmail.com>
2026-02-13 21:09:53 +0300

    Overload method örnekler

---
+++ 260213_1_Overload_Method/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace _260213_1_Overload_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Overload Method => Aşırı yüklenmiş method.
+            // Aynı isimde birden fazla işlevi gören methodlara overload method (aşırı yüklenmiş method) denir.
+            /*
+            
+             Aşırı yüklenmiş methodlar aşağıdaki kurallara göre yapılmalıdır:
+            1) Aynı isimle olmalıdır.
+            2) Farklı sayıda parametreye sahip olmalıdır. Eğer aynı sayıda parametre içeren başka overload method varsa parametre tiplerinden en az birinin farklı olması gereklidir.
+            3) void ya da return olması overload özelliğini değiştirmez.
+
+            */
+
+            Console.WriteLine();
+            Console.Write(true);
+            Matematik("");
+            
+            int s1 = Matematik();
+            int s2 = Matematik();
+            Matematik(s1, s2);
+            Console.WriteLine("2 sayının toplamı:"+ Matematik(s1,s2));
+
+            double d1 = Matematik();
+            double d2 = Matematik();
+            Matematik(d1, d2);
+            // Console.WriteLine("Küçük sayıdan büyük sayıya kadar olan sayıların toplamı:" + Matematik(d1, d2));
+            Console.WriteLine("Küçük sayıdan büyük sayıya kadar olan sayıların toplamı:" + Matematik((double)s1, (double)s2));
+
+
+        }
+
+        /// <summary>
+        /// Sayı girişi
+        /// </summary>
+        /// <returns></returns>
+        static int Matematik()
+        {
+            Matematik("Sayı giriniz:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Ekrana girilen metni verir.
+        /// </summary>
+        /// <param name="metin"></param>
+        static void Matematik(string metin) // Ekrana yazı yazar.
+        {
+            Console.WriteLine(metin);
+        }
+
+
+        /// <summary>
+        /// test-1
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static void Matematik(int text)
+        {
+            // İsim aynı, parametre aynı ama parametre tipi diğerinden farklı. Overload method özelliği taşır.
+        }
+
+        /// <summary>
+        /// test-2
+        /// </summary>
+        /// <param name="text"></param>
+        static void Matematik(double text)
+        {
+            // İsim aynı, parametre aynı ama parametre tipi diğerinden farklı. Overload method özelliği taşır.
+        }
+
+        /// <summary>
+        /// 2 sayının toplamını verir.
+        /// </summary>
+        /// <param name="sayi1"></param>
+        /// <param name="sayi2"></param>
+        /// <returns></returns>
+        static int Matematik(int sayi1,int sayi2)
+        {
+            // İsim aynı, 2 parametre içerir ve 2 int tipinde parametre içeren aynı isimdeki methodlarda yok, overload özelliği taşır.
+            return sayi1+ sayi2;
+        }
+
+        /// <summary>
+        /// 2 sayı arasındaki sayıların toplamını hesaplar.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        static double Matematik(double s1,double s2)
+        {
+            double toplam = 0;
+
+            if (s1>s2)
+            {
+                for (double i = s2; i < s1; i++)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            else
+            {
+                for (double i = s1; i<s2; i++)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            return toplam;
+        }
+
+    }
+}
---
+++ 260213_2_Overload_Ornek1/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _260213_2_Overload_Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan alınan 2 sayı için DortIslem() adında bir method kodlayınız.
+            /*
+            
+            1) Bu 2 sayının toplamını DortIslem() adında method ile hesaplayınız. 
+            2) Bu 2 sayının çarpımını DortIslem() adında method ile hesaplayınız. 
+            3) Bu 2 sayının farkını DortIslem() adında method ile hesaplayınız. 
+            4) Büyük sayının küçük sayıya bölümünde kalanı veren DortIslem() adında bir method kodlayınız.
+            5) Büyük sayının küçük sayıya bölümünde bölümü veren DortIslem() adında bir method kodlayınız.
+
+            */
+
+            int s1 = DortIslem();
+            int s2 = DortIslem();
+
+            DortIslem("2 sayının toplamı:" + DortIslem(s1, s2));
+            DortIslem("2 sayının çarpımı:" + DortIslem((long)s1, (long)s2));
+            DortIslem("2 sayının farkı:" + DortIslem((short)s1, (short)s2));
+            DortIslem("Büyük sayının küçük sayıya bölümünde kalan:" + DortIslem((double)s1, (double)s2));
+            DortIslem("Büyük sayının küçük sayıya bölüm sonucu:" + DortIslem((decimal)s1, (decimal)s2));
+            
+        }
+
+        static int DortIslem()
+        {
+            Console.WriteLine("Sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            return sayi;
+        }
+
+        static int DortIslem(int sayi1,int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            return toplam;
+        }
+
+        static long DortIslem(long sayi1,long sayi2)
+        {
+            long carpim = sayi1 * sayi2;
+            return carpim;
+        }
+        static long DortIslem(short sayi1,short sayi2)
+        {
+            long fark = sayi1 - sayi2;
+            return fark;
+        }
+        
+        static double DortIslem(double deger1,double deger2)
+        {
+            if (deger1>deger2)
+            {
+                return deger1 % deger2;
+            }
+            else
+            {
+                return deger2 % deger1;
+            }
+        }
+
+        static decimal DortIslem(decimal deger1,decimal deger2)
+        {
+            if (deger1>deger2)
+            {
+                return deger1 / deger2;
+            }
+            else
+            {
+                return deger2 / deger1;
+            }
+        }
+
+        static void DortIslem(string metin)
+        {
+            Console.WriteLine(metin);
+        }
+
+
+
+    }
+}
